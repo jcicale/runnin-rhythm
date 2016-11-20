c24dafe296037dff4d5cd100264a86c546dbd967
Julia <juliacicale1@julia-cicales-macbook-pro.local>
2016-11-20 09:58:06 -0600

    updates to playlist view, testing api calls

--- public/js/account-home.js
+++ public/js/account-home.js
@@ -1,3 +1,4 @@
+//filter checkbox
 $('.clear-all').on('click', function(){
 	$('input:checkbox[name=cbox1]').attr('checked', false);
 	$('input:checkbox[name=cbox2]').attr('checked', false);
@@ -6,62 +7,158 @@ $('.clear-all').on('click', function(){
 	$('input:checkbox[name=cbox5]').attr('checked', false);
 });
 
-
+//filter slider
 function updateLengthFilter(val) {
     document.getElementById('slider-value').value=val; 
 };
 
 function updateSlider(val) {
-    document.getElementById('slider').value=val; 
+    document.getElementById('slider-filter').value=val; 
 };
 
+//playlist slide panel
+var slider = $('#slider').slideReveal({
+	position: "right",
+	top: 66,
+	width: '65%',
+	push: false,
+	overlay: true,
+	overlayColor: 'rgba(0,0,0,.5)'
+});
+
+//dynamically add playlists
 function addPlaylist() {
 		var $background = $("<div>");
 		var $cover = $("<div>");
-		var $information = $("<div><h4>Groovin' 5K</h4><h5>Pace: 9:30</h5><h5>Length: 3.1 Miles</h5><h5>Genre: Pop</h5></div>");
-		$('#playlist-container').append($background);
+		var playlistObject = {
+			name: "Groovin' 5K",
+			pace: "9:30",
+			length: "3.1 Miles",
+			genre: "Pop"
+		};
+		function addInformation(playlistObject) {
+			var $information = $("<div></div>");
+			$information.append("<h4>" + playlistObject.name +"</h4>");
+			$information.append("<h5>" + playlistObject.pace +"</h5>");
+			$information.append("<h5>" + playlistObject.length +"</h5>");
+			$information.append("<h5>" + playlistObject.genre +"</h5>");
+			$information.addClass("playlist-info");
+			return $information;
+		}
+		$('#playlists-container').append($background);
 		$background.append($cover);
 		$cover.addClass("flex-item artwork-cover")
 		$background.addClass("background flex-item");
 		$background.css("background-image", "url('images/album-art/adele.jpg')");
-		$cover.append($information);
-		$information.addClass("playlist-info");
+		$cover.append(addInformation(playlistObject));
+
+		$cover.on('click', function() {
+			slider.slideReveal("toggle");
+		});
+
 	}
 
 $('#add-playlist').on('click', function(){
 	addPlaylist();
 });
 
-function loadUser() {
-	function getUrlParameter(sParam) {
-	    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-	        sURLVariables = sPageURL.split('&'),
-	        sParameterName,
-	        i;
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
-	    for (i = 0; i < sURLVariables.length; i++) {
-	        sParameterName = sURLVariables[i].split('=');
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
 
-	        if (sParameterName[0] === sParam) {
-	            return sParameterName[1] === undefined ? true : sParameterName[1];
-	        }
-	    }
-	};
-
-    var access_token = getUrlParameter('access_token');
-
-	$.ajax({
-    url: 'https://api.spotify.com/v1/me',
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    success: function(response) {
-      $('#avatar').attr('src', response.images[0].url);
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
     }
-});
+};
+
+function loadAll() {
+ loadUser();
+ loadSongs();
 }
 
-$(document).ready(loadUser);
+function loadSongs() {
+	var access_token = getUrlParameter('access_token');
+	$.ajax({
+    	url: 'https://api.spotify.com/v1/search',
+    	headers: {
+      		'Authorization': 'Bearer ' + access_token
+    	},
+    	success: function(response) {
+      		getAudioProfile(response.tracks.items);
+    	},
+    	data: {
+    		"q" : "genre:Electronic",
+    		"type" : "track",
+    		"market" : "US",
+    		"limit" : "50"
+    	}
+	});
+}
+
+function getTrackIDsFromSpotifyTracks(tracks) {
+	return tracks.map(function(track) {
+		return track.id;
+	})
+}
+
+function getAudioProfile(tracks) {
+	var trackIDs = getTrackIDsFromSpotifyTracks(tracks);
+	var access_token = getUrlParameter('access_token');
+	$.ajax({
+    	url: 'https://api.spotify.com/v1/audio-features',
+    	headers: {
+      		'Authorization': 'Bearer ' + access_token
+    	},
+    	success: function(response) {
+      		var audioProfileDictionary = convertAudioProfilesToDictionary(response.audio_features);
+      		var tracksWithAudioProfiles = addAudioProfileToTrack(tracks, audioProfileDictionary);
+      		console.log(tracksWithAudioProfiles);
+    	},
+    	data: {
+    		"ids" : trackIDs.join()
+    	}
+	});
+}
+
+function convertAudioProfilesToDictionary(audioProfiles) {
+	var dictionary = {};
+	for (var i = 0; i < audioProfiles.length; i++) {
+		var profile = audioProfiles[i];
+		dictionary[profile.id] = profile;
+	}
+	return dictionary;
+}
+
+function addAudioProfileToTrack(tracks, audioProfiles) {
+	for (var i = 0; i < tracks.length; i++) {
+		var track = tracks[i];
+		track.audioProfile = audioProfiles[track.id];
+	}
+	console.log(tracks);
+	return tracks;
+}
+
+//get user info from spotify
+function loadUser() {
+    var access_token = getUrlParameter('access_token');
+	$.ajax({
+    	url: 'https://api.spotify.com/v1/me',
+    	headers: {
+      		'Authorization': 'Bearer ' + access_token
+    	},
+    	success: function(response) {
+      		$('#avatar').attr('src', response.images[0].url);
+    	}
+	});
+}
+
+$(document).ready(loadAll);
 
 
 
