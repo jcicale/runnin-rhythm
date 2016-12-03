fc961a6593a20df7ef0aaf13aa00e01ce2125ccc
Julia <juliacicale1@julia-cicales-macbook-pro.local>
2016-12-03 16:54:39 -0600

    fixed dropdown, added validation for modal, added spinner

--- public/js/account-home.js
+++ public/js/account-home.js
@@ -1,3 +1,77 @@
+var genres = ["Alternative", "Christian", "Country", "EDM/Dance", "Folk", "Hip-Hop", "Indie", "Jazz", "Metal",  "Party", "Pop", "RnB", "Rock", "Soul" ];
+var paces = ["6:00", "6:15", "6:30", "6:45", "7:00", "7:15", "7:30", "7:45", "8:00", "8:15", "8:30", "8:45", "9:00", "9:15", "9:30", "9:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00"];
+
+function createGenreClickListener(i) {
+	return function() {
+		$("#filter-genre-dropdown-title").html(genres[i]);
+		$(".dropdown").blur();
+	};
+};
+
+function createModalGenreClickListener(i) {
+	return function() {
+		$("#modal-genre-dropdown-title").html(genres[i]);
+		$(".dropdown").blur();
+	};
+};
+
+function createPaceClickListener(i) {
+	return function() {
+		$("#modal-pace-dropdown-title").html(paces[i] + " min");
+		$(".dropdown").blur();
+	};
+};
+
+function populateDropdowns() {
+	var genreLength = genres.length;
+	for (var i = 0; i < genreLength; i ++) {
+		var $listItem = $("<li></li>");
+		var $button = $("<button>");
+		var genre = genres[i];
+		$button.html(genres[i]);
+		$listItem.append($button);
+		$("#filter-genre-dropdown ul").append($listItem);
+		$listItem.on("click", createGenreClickListener(i));
+	}
+	for (var i = 0; i < genreLength; i ++) {
+		var $listItem = $("<li></li>");
+		var $button = $("<button>" + genres[i] + "</button>");
+		$listItem.append($button);
+		$("#modal-genre-dropdown ul").append($listItem);
+		$listItem.on("click", createModalGenreClickListener(i));
+	}
+	var paceLength = paces.length;
+	for (var i = 0; i < paceLength; i ++) {
+		var $listItem = $("<li></li>");
+		var $button = $("<button>" + paces[i] + " min" + "</button>");
+		$listItem.append($button);
+		$("#modal-pace-dropdown ul").append($listItem);
+		$listItem.on("click", createPaceClickListener(i));
+	}
+};
+
+//generate playlist button
+$("#generate-playlist").on("click", function(){
+	var playlistName = $("#playlist-name").val();
+	var runLengthSelection = $("#run-length").val();
+	var paceSelection = $("#modal-pace-dropdown-title").html();
+	var genreSelection = $("#modal-genre-dropdown-title").html();
+	if (playlistName == "" || paceSelection == "Select Pace" || genreSelection == "Select Genre") {
+		$("#create-playlist-modal").append("<p>Please enter a value for all fields to continue</p>");
+	}
+	else {
+		$("#modal-container").hide();
+		var spinner = new Spinner({color: "#fff"}).spin();
+		$("#create-playlist-modal").append(spinner.el);
+		$("#create-playlist-modal").modal({
+  			escapeClose: false,
+ 			clickClose: false,
+  			showClose: false
+		});
+		}
+		//$.modal.close();
+});
+
 //filter checkbox
 $('.clear-all').on('click', function(){
 	$('input:checkbox[name=cbox1]').attr('checked', false);
@@ -82,6 +156,7 @@ function getUrlParameter(sParam) {
 function loadAll() {
  loadUser();
  loadSongs();
+ populateDropdowns();
 }
 
 function loadSongs() {
